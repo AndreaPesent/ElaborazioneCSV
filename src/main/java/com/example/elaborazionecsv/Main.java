89d9a0a05b0ef2ac96830dd53c738ea9c63c38c3
pesenti.21136 <pesenti.andrea.studente67@itispaleocapa.it>
2025-04-14 13:50:47 +0200

    metodo nuovo file html

--- src/main/java/com/example/elaborazionecsv/Main.java
+++ src/main/java/com/example/elaborazionecsv/Main.java
@@ -4,61 +4,66 @@ import java.util.Random;
 
 public class Main
 {
-    public static void main (String[]args) throws IOException
+    public static void main (String[]args)
     {
-        FileReader fr = new FileReader("C:\\Users\\pesenti.21136\\Desktop\\Pesenti.csv");
-        BufferedReader br = new BufferedReader(fr);
-        BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\pesenti.21136\\Desktop\\Pesenti.csv"));
-        String next;
+        html();
     }
     public void MioCampo(BufferedWriter bw, BufferedReader br, String next)
     {
-        try
-        {
-            next="*";
-            do
-            {
-                Random a = new Random();
-                br.readLine();
-                bw.write(";MioValore");
 
-            } while (next!=null);
-        } catch (Exception e)
-        {
-            throw new RuntimeException(e);
-        }
     }
     public void ContaCampi (BufferedReader br, String next)
     {
-        int cont=0;
-        try
-        {
-            do
-            {
-                br.readLine();
-                if(next==";")
-                {
-                    cont++;
-                }
-                br.close();
-            } while (next!=null);
-            System.out.println("numero di campi: " + cont);
-        } catch (Exception e)
-        {
-            throw new RuntimeException(e);
-        }
+
     }
     public void AggiuntaRecord (BufferedWriter bw, BufferedReader br, String next)
     {
-        try
-        {
-            next="*";
-            do
-            {
-                br.readLine();
-                bw.write(";false");
 
-            } while (next!=null);
+    }
+    public static void html()
+    {
+        try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\pesenti.21136\\Desktop\\Pesenti.csv"));
+        PrintWriter pw = new PrintWriter(new FileWriter("C:\\Users\\pesenti.21136\\Desktop\\index.html")))
+        {
+            pw.print("<!DOCTYPE html>\n" +
+                    "<html lang=\"en\">\n" +
+                    "<head>\n" +
+                    "    <meta charset=\"UTF-8\">\n" +
+                    "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                    "    <title>Document</title>\n" +
+                    "</head>\n" +
+                    "<body>\n"+
+                    "<table>\n");
+            String line=br.readLine();
+            String[] valori=line.split(";");
+            pw.print("<tr>\n" +
+                    "    <th>"+valori[0]+"</th>\n" +
+                    "    <th>"+valori[1]+"</th>\n" +
+                    "    <th>"+valori[2]+"</th>\n" +
+                    "  </tr>\n");
+            while((line=br.readLine())!=null)
+            {
+                valori=line.split(";");
+                pw.print("<tr>\n" +
+                        "    <td>"+valori[0]+"</td>\n" +
+                        "    <td>"+valori[1]+"</td>\n" +
+                        "    <td>"+valori[2]+"</td>\n" +
+                        "    <td>"+valori[3]+"</td>\n" +
+                        "    <td>"+valori[4]+"</td>\n" +
+                        "    <td>"+valori[5]+"</td>\n" +
+                        "    <td>"+valori[6]+"</td>\n" +
+                        "    <td>"+valori[7]+"</td>\n" +
+                        "    <td>"+valori[8]+"</td>\n" +
+                        "    <td>"+valori[9]+"</td>\n" +
+                        "    <td>"+valori[10]+"</td>\n" +
+                        "    <td>"+valori[11]+"</td>\n" +
+                        "    <td>"+valori[12]+"</td>\n" +
+                        "    <td>"+valori[13]+"</td>\n" +
+                        "    <td>"+valori[14]+"</td>\n" +
+                        "  </tr>\n");
+            }
+            pw.print("</table>\n</body>\n</html>");
         } catch (Exception e)
         {
             throw new RuntimeException(e);
